63479e83c0e3f4d601cfcfb214e433f26296ccef
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2022-12-10 16:16:18 +0700

    Initial Update

--- database/migrations/2022_12_10_090203_create_images_table.php
+++ database/migrations/2022_12_10_090203_create_images_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('image__products', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('image__products');
+        Schema::dropIfExists('images');
     }
 };
